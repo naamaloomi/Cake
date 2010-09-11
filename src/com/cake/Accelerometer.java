56cc76fd57d66aa8d2ea4195897c36d170f81e01
Niklas Bolander <niklas.bolander@gmail.com>
2010-09-11 15:15:15 +0200

    Added accelerometer support.

---
+++ src/com/cake/Accelerometer.java
@@ -0,0 +1,74 @@
+package com.cake;
+
+import java.util.List;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+/**
+ * Simple class for accessing the x, y, z values of the accelerometer in a static way
+ */
+public final class Accelerometer {
+	private static SensorManager sensorManager;
+    private static Sensor sensor;
+    private static boolean started = false;
+    
+    private static float x;
+    private static float y;
+    private static float z;
+    
+    private static SensorEventListener listener = new SensorEventListener() {
+    	
+    	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    		// Part of the interface but not used here
+    	}
+
+    	public void onSensorChanged(SensorEvent event) {
+    		x = event.values[0];
+    		y = event.values[1];
+    		z = event.values[2];
+    	}
+    };
+    
+	private Accelerometer() {
+	}
+
+    public static float getX() {
+    	return x;
+    }
+    
+    public static float getY() {
+    	return y;
+    }
+    
+    public static float getZ() {
+    	return z;
+    }
+    
+	public static void start(SensorManager sensorManager) {
+		if (started) {
+			return;
+		}
+		if (sensor == null) {
+	        List<Sensor> sensors = sensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER);
+	        if (sensors.size() == 0)
+	        	throw new IllegalStateException("No accelerometer available");
+
+	        sensor = sensors.get(0);
+	        Accelerometer.sensorManager = sensorManager;
+		}
+    	sensorManager.registerListener(listener, sensor, SensorManager.SENSOR_DELAY_GAME);
+		started = true;
+	}
+	
+	public static void stop() {
+		if (!started) {
+			return;
+		}
+		started = false;
+		
+		sensorManager.unregisterListener(listener);
+	}
+}
--- src/com/cake/CakeActivity.java
+++ src/com/cake/CakeActivity.java
@@ -1,24 +1,40 @@
 package com.cake;
 
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
 public class CakeActivity extends Activity
 {
-
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-    	makeFullScreen();
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+    public void onCreate(Bundle savedInstanceState) {
+		makeFullScreen();
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
     }
 
-    private void makeFullScreen() {
-    	requestWindowFeature(Window.FEATURE_NO_TITLE);
-	getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-    }
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+		Accelerometer.start(sensorManager);
+	}
+
+	@Override
+	protected void onPause() {
+			super.onPause();
+			Accelerometer.stop();
+	}
+
+
+	private void makeFullScreen() {
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+	}
+
 }
