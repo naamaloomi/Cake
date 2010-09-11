02dd87c09b2b22b5a737b881dae23f694586ca3e
Niklas Bolander <niklas.bolander@gmail.com>
2010-09-11 11:39:34 +0200

    Now with buildable code!

--- src/com/cake/CakeActivity.java
+++ src/com/cake/CakeActivity.java
@@ -2,21 +2,23 @@ package com.cake;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class CakeActivity extends Activity
 {
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
-    	makeFullscreen();
+    	makeFullScreen();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
 
-    prive void makeFullScreen() {
+    private void makeFullScreen() {
     	requestWindowFeature(Window.FEATURE_NO_TITLE);
 	getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
-
 }
