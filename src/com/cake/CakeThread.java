7cfd3af293811ba3e364a95b1068bd6537549d9d
Kristina Nylander <kristina.nylander@gmail.com>
2010-09-11 19:09:15 +0200

    Brick exists

--- src/com/cake/CakeThread.java
+++ src/com/cake/CakeThread.java
@@ -21,10 +21,12 @@ class CakeThread extends Thread {
 	private final float dt = 0.1f;
 	public CakeThread(SurfaceHolder surfaceHolder, CakeView panel, Context context) {
  	    bricks = new ArrayList<Body>(10);
+		bricks.add(new Brick(10, 10, new Paint(), BitmapFactory.decodeResource(context.getResources(), R.drawable.brick1)));
 		ball = new Ball(50,50, new Paint(), BitmapFactory.decodeResource(context.getResources(), R.drawable.ball));
 		ball.vel_x = 5;
 		ball.vel_y = 10;
 		paddle = new Paddle(120, new Paint(), BitmapFactory.decodeResource(context.getResources(), R.drawable.paddel_basic));
+		ball.vel_x = 3;
 		this.surfaceHolder = surfaceHolder;
 		view = panel;
 	}
@@ -43,6 +45,10 @@ class CakeThread extends Thread {
 				c = surfaceHolder.lockCanvas(null);
 				synchronized (surfaceHolder) {
 					c.drawRGB(0,0,0);
+					for (Body b : bricks) {
+						b.update(dt);
+						b.draw(c);
+					}
 					paddle.update(dt);
 					paddle.draw(c);
 					ball.update(dt);
